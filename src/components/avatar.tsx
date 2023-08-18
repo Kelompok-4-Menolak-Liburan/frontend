80c411ce7a1e723184a2126697eb8677437b48f2
fairuzald <fairuzy210@gmail.com>
2023-08-18 13:47:42 +0700

    feat: Implementation of the avatar component based on figma design

---
+++ src/components/avatar.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+interface AvatarProps {
+  size: "small" | "normal" | "large";
+  name?: string;
+  role?: "Customer" | "Event Organizer" | "Administrator";
+  imageUrl: string;
+}
+const Avatar: React.FC<AvatarProps> = ({ size, name, role, imageUrl }) => {
+  // Define styles and dimensions based on avatar size
+  const sizeEffectStyle = {
+    small: {
+      imageStyle: "w-[40px] h-[40px]",
+      primaryTextStyle: "text-xs",
+      childTextStyle: "text-[10px]",
+    },
+    normal: {
+      imageStyle: "w-[50px] h-[50px]",
+      primaryTextStyle: "text-base",
+      childTextStyle: "text-[11px]",
+    },
+    large: {
+      imageStyle: "w-[75px] h-[75px]",
+      primaryTextStyle: "text-[13px]",
+      childTextStyle: "text-[11px]",
+    },
+  };
+
+  // Define image dimensions based on avatar size
+  const sizeEffectImage = {
+    small: 40,
+    normal: 50,
+    large: 75,
+  };
+
+  return (
+    <div className="flex gap-4 items-center justify-center">
+      <Image
+        src={imageUrl}
+        alt={name + " Avatar"}
+        width={sizeEffectImage[size]}
+        height={sizeEffectImage[size]}
+        className={`rounded-full aspect-square object-center object-cover ${sizeEffectStyle[size].imageStyle}`}
+      />
+      {/* Render name and role if provided */}
+      {name && role && (
+        <div className="flex flex-col justify-center font-poppins text-white">
+          <p className={`font-bold ${sizeEffectStyle[size].primaryTextStyle}`}>
+            {name}
+          </p>
+          <p className={sizeEffectStyle[size].childTextStyle}>as a {role}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+export default Avatar;
