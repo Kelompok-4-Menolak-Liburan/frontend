2927914cc096f6f650a138c2c0a7662b8bb5ad30
fairuzald <fairuzy210@gmail.com>
2023-08-19 17:15:05 +0700

    feat: Adding metadata, creating layouting, and page regarding registration based on figma design

---
+++ src/app/(auth)/register/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next"
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  return <body>{children}</body>
+}
+export const metadata: Metadata = {
+  title: "Registration | SeTiket",
+  description:
+    "Registration",
+  generator: "Next.js",
+  applicationName: "SeTiket",
+  colorScheme: "dark"
+}
