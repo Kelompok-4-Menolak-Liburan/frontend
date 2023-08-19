5c36c6762b034c8fcf02a7c18cdbed66808a5211
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-19 17:27:24 +0700

    Register Page & Text Input (#9)
    * feat: Adding an image for the need to create a registration page
    
    * feat: Create a text-input component that serves search, email, text, and password types based on the figma design
    
    * feat: Prepare file structure for multiple layouts
    
    * feat: Adding metadata, creating layouting, and page regarding registration based on figma design
    
    * style: Make it more responsive, customizing the design for all viewports

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,23 +1,24 @@
-"use client"
 import Switch from '@/components/switch';
 import EventDetailsTabs from '@/components/tabs/event-details-tabs';
+import { Metadata } from 'next';
 import React, { useState } from 'react';
 export default function Home() {
-  const [isChecked, setIsChecked] = useState(false);
 
-  // Handler to update the state when the switch is toggled
-  const handleSwitchChange = (newChecked: boolean) => {
-    setIsChecked(newChecked);
-  };
+
   return (
     <main className="flex min-h-screen flex-col gap-2 items-center justify-between text-white p-24 font-poppins">
       <div className="w-[1000px]">
         <EventDetailsTabs />
-        <Switch
-          checked={isChecked} // Pass the current state as checked
-          onChange={handleSwitchChange} // Pass the handler to update the state
-        />
       </div>
     </main>
   );
+}
+
+export const metadata: Metadata = {
+  title: "SeTiket",
+  description:
+    "Registration",
+  generator: "Next.js",
+  applicationName: "SeTiket",
+  colorScheme: "dark"
 }
