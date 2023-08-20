d535fe7f3aa22afa5ac0eba566160e2f72b1f761
fairuzald <fairuzald@users.noreply.github.com>
2023-08-20 16:41:39 +0000

    Prettified Code!

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 import Dropdown from "@/components/drop-down";
 import { Metadata } from "next";
 import React, { useState } from "react";
 export default function Home() {
-  const options = ['Option 1', 'Option 2', 'Option 3'];
-  const [selectedOption, setSelectedOption] = useState('');
+  const options = ["Option 1", "Option 2", "Option 3"];
+  const [selectedOption, setSelectedOption] = useState("");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-2 p-24 font-poppins text-white">
       <div className="w-[300px]">
