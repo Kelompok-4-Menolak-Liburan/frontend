ab6952ad95c8bab4bfbeb7ac1c37a769498ef72e
fairuzald <fairuzald@users.noreply.github.com>
2023-08-20 18:09:51 +0000

    Prettified Code!

--- src/app/(site)/loading.tsx
+++ src/app/(site)/loading.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 
 const Loading = () => {
-    return (
-        <main className="flex flex-auto items-center justify-center flex-col h-screen gap-8">
-            <Image src="/logo.png" width={250} height={250} alt="Logo" className="w-[150px] lg:w-[200px] 2xl:w-[250px] aspect-square" />
-            <h1 className="font-poppins text-[#C5c5c5] text-4xl lg:text-6xl 2xl:text-[80px] font-bold">SeTiket</h1>
-        </main>
-    );
+  return (
+    <main className="flex h-screen flex-auto flex-col items-center justify-center gap-8">
+      <Image
+        src="/logo.png"
+        width={250}
+        height={250}
+        alt="Logo"
+        className="aspect-square w-[150px] lg:w-[200px] 2xl:w-[250px]"
+      />
+      <h1 className="font-poppins text-4xl font-bold text-[#C5c5c5] lg:text-6xl 2xl:text-[80px]">
+        SeTiket
+      </h1>
+    </main>
+  );
 };
 
 export default Loading;
