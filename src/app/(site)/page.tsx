52788e24e658257b06ccd740dcf2dbd2f7dad242
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-20 23:42:21 +0700

    Dropdown (#12)
    * feat: Apply a dropdown component based on figma style
    
    * feat: Remove metadata on client component
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: fairuzald <fairuzald@users.noreply.github.com>

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,21 +1,20 @@
-import Switch from "@/components/switch";
-import EventDetailsTabs from "@/components/tabs/event-details-tabs";
+"use client";
+import Dropdown from "@/components/drop-down";
 import { Metadata } from "next";
 import React, { useState } from "react";
 export default function Home() {
+  const options = ["Option 1", "Option 2", "Option 3"];
+  const [selectedOption, setSelectedOption] = useState("");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-2 p-24 font-poppins text-white">
-      <div className="w-[1000px]">
-        <EventDetailsTabs />
+      <div className="w-[300px]">
+        <Dropdown
+          options={options}
+          placeholder="Select an option"
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+        />
       </div>
     </main>
   );
 }
-
-export const metadata: Metadata = {
-  title: "SeTiket",
-  description: "Registration",
-  generator: "Next.js",
-  applicationName: "SeTiket",
-  colorScheme: "dark",
-};
