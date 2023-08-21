25873262c5dd72a6d0ae3e01cc0dc733e453dd43
FajarWG <fajarwahyugumelar@gmail.com>
2023-08-21 15:02:11 +0700

    chores: fix size button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -51,7 +51,7 @@ const Button = ({
       aria-label={
         typeof children === "string" ? `${children} Button` : "Button"
       }
-      className={`rounded-full font-poppins text-lg font-bold transition duration-300 hover:opacity-80 disabled:cursor-not-allowed ${
+      className={`rounded-full font-poppins text-base font-bold transition duration-300 hover:opacity-80 disabled:cursor-not-allowed ${
         fullWidth ? "w-full" : "w-fit"
       } ${sizeEffect[size]} ${colorStyle[color]} `}
     >
