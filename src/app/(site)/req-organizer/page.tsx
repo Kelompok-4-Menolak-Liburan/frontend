68b09a3961f2a0a6ce11e31cdbf097b55e176b30
FajarWG <fajarwahyugumelar@gmail.com>
2023-08-22 20:30:44 +0700

    feat: add tooltip if rejected

--- src/app/(site)/req-organizer/page.tsx
+++ src/app/(site)/req-organizer/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import Avatar from "@/components/avatar";
 import Button from "@/components/button";
-import Image from "next/image";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 import Header from "@/components/header";
 import React, { useState } from "react";
 import TextInput from "@/components/text-input";
-import { DocumentUpload } from "iconsax-react";
+import { DocumentUpload, InfoCircle } from "iconsax-react";
 
 export default function EventDetail() {
   const [search, setSearch] = useState("");
@@ -26,7 +25,29 @@ export default function EventDetail() {
         avatarName="Tes"
         avatarRole="Customer"
       />
-      <h2 className="py-8 text-2xl font-semibold">Request Event Organizer</h2>
+      <h2 className="py-8 text-2xl font-semibold">
+        Request Event Organizer -{" "}
+        <span
+          className="inline-flex cursor-pointer gap-1 text-lg font-bold text-red-500"
+          data-tooltip-id="my-tooltip-2"
+        >
+          Rejected
+          <InfoCircle size={18} color="#ef4444" />
+        </span>
+      </h2>
+
+      <ReactTooltip
+        id="my-tooltip-2"
+        place="bottom"
+        variant="info"
+        style={{
+          wordBreak: "break-all",
+          overflowWrap: "break-word",
+          width: 500,
+          backgroundColor: "#1e223e",
+        }}
+        content="Karena lorem ipsum dolor sit amet Karena lorem ipsum dolor sit amet Karena lorem ipsum dolor sit amet Karena lorem ipsum dolor sit amet Karena lorem ipsum dolor sit amet Karena lorem ipsum dolor sit amet Karena lorem ipsum dolor sit amet Karena lorem ipsum dolor sit amet"
+      />
       <div className="flex w-full flex-col gap-10 lg:flex-row">
         <div className="flex w-full flex-col">
           <div className="flex flex-col gap-2">
@@ -70,13 +91,17 @@ export default function EventDetail() {
               <h3 className="mt-5 font-semibold leading-[135%] tracking-wider text-white selection:text-sm lg:text-base 2xl:text-lg">
                 Uploud Proposal
               </h3>
-              <label className="hover:bg-blue flex w-full cursor-pointer flex-col items-center rounded-lg bg-custom-purple-300 px-4 py-6  tracking-wide shadow-lg hover:text-custom-purple-100">
-                <DocumentUpload variant="Linear" size="24" color="#fff" />
-                <span className="mt-2 text-center text-xs lg:text-base">
-                  Click or drag file to this area to uploud proposal
-                </span>
-                <input type="file" className="hidden" />
-              </label>
+              <input
+                type="file"
+                className="block w-full rounded-full border
+                            border-slate-500 text-sm text-slate-500
+                            file:mr-4 file:rounded-full
+                            file:border-0 file:bg-violet-50
+                            file:px-4 file:py-2
+                            file:text-sm
+                            file:font-semibold file:text-custom-purple-300 hover:file:bg-violet-100
+                            "
+              />
             </div>
             <div className="mt-5 self-center">
               <Button color="green-primary" size="small">
