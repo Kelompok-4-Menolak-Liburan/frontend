48cc2b243129090e9ee58c0ddd45a84e4570b37e
fairuzald <fairuzy210@gmail.com>
2023-08-23 16:51:00 +0700

    feat: Implement all pages using existing components based on figma styling

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,14 +1,23 @@
+"use client"
 import Sidebar from "@/components/sidebar/sidebar";
+import { createContext, useState } from "react";
+
+export const PopUp = createContext({});
+
 
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
+  const [popUp, setPopUp] = useState<React.ReactNode | undefined>();
   return (
     <body>
-      <div className="flex h-screen w-full bg-custom-purple-400">
-        <Sidebar />
-        <div className="max-lg:w-screen lg:flex-1 lg:pl-[226px] xl:pl-[253px]">
-          {children}
+      <PopUp.Provider value={setPopUp}>
+        <div className="flex h-screen w-full bg-custom-purple-400">
+          <Sidebar />
+          <div className="max-lg:w-screen lg:flex-1 lg:pl-[226px] xl:pl-[253px]">
+            {children}
+            {popUp}
+          </div>
         </div>
-      </div>
+      </PopUp.Provider>
     </body>
   );
 };
