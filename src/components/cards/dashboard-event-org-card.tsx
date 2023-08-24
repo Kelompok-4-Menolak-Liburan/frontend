a73767d8effe4b2ed7726b374965e4c187d078a0
fairuzald <fairuzald@users.noreply.github.com>
2023-08-24 12:36:07 +0000

    Prettified Code!

--- src/components/cards/dashboard-event-org-card.tsx
+++ src/components/cards/dashboard-event-org-card.tsx
@@ -1,19 +1,30 @@
 interface CardDashboardProps {
-    icon: JSX.Element;
-    unit: string;
-    amount: number;
-    placeholder: string;
-
+  icon: JSX.Element;
+  unit: string;
+  amount: number;
+  placeholder: string;
 }
-const CardDashboard: React.FC<CardDashboardProps> = ({ icon, placeholder, amount, unit }) => {
-    return <div className="w-[150px] lg:w-[200px] items-center justify-center aspect-square bg-custom-purple-500 rounded-xl flex flex-col gap-4 px-5 py-6 font-poppins">
-        {icon}
-        <div className="flex flex-col w-full">
-            <p className="text-custom-purple-75 text-sm lg:text-base">{placeholder}</p>
-            <div className="flex gap-1">
-                <p className="text-white text-sm lg:text-base"><span className="text-base lg:text-xl font-bold">{amount} </span>{" " + unit}</p>
-            </div>
+const CardDashboard: React.FC<CardDashboardProps> = ({
+  icon,
+  placeholder,
+  amount,
+  unit,
+}) => {
+  return (
+    <div className="flex aspect-square w-[150px] flex-col items-center justify-center gap-4 rounded-xl bg-custom-purple-500 px-5 py-6 font-poppins lg:w-[200px]">
+      {icon}
+      <div className="flex w-full flex-col">
+        <p className="text-sm text-custom-purple-75 lg:text-base">
+          {placeholder}
+        </p>
+        <div className="flex gap-1">
+          <p className="text-sm text-white lg:text-base">
+            <span className="text-base font-bold lg:text-xl">{amount} </span>
+            {" " + unit}
+          </p>
         </div>
+      </div>
     </div>
-}
-export default CardDashboard
+  );
+};
+export default CardDashboard;
--- src/components/table/ticket-list.tsx
+++ src/components/table/ticket-list.tsx
@@ -2,57 +2,57 @@ import { Chart, Edit2, Eye } from "iconsax-react";
 import Image from "next/image";
 
 interface TicketListProps {
-    no: number;
-    imageUrl: string;
-    eventName: string;
-    status: "active" | "not active";
-    sold: number;
-    view: number;
+  no: number;
+  imageUrl: string;
+  eventName: string;
+  status: "active" | "not active";
+  sold: number;
+  view: number;
 }
 
 const TicketList: React.FC<TicketListProps> = ({
-    no,
-    imageUrl,
-    eventName,
-    status,
-    sold,
-    view,
+  no,
+  imageUrl,
+  eventName,
+  status,
+  sold,
+  view,
 }) => {
-    return (
-        <tr
-            key={no}
-            className="w-full overflow-x-scroll rounded-[20px] bg-custom-purple-300 font-poppins text-sm text-white lg:text-base"
-        >
-            {/* Numbering */}
-            <td className="py-3">{no}</td>
-            {/* Image */}
-            <td className="w-[120px] py-3">
-                <Image
-                    alt={eventName + " Image"}
-                    src={imageUrl}
-                    width={120}
-                    height={60}
-                    className="h-[60px] w-[120px] rounded-[10px] object-cover object-center"
-                />
-            </td>
-            {/* Event Name */}
-            {/* Event Name */}
-            <td className="min-w-[140px] px-2 py-3">{eventName}</td>
-            {/* Status */}
-            <td
-                className={`capitalize ${status.toLowerCase() === "active"
-                    ? "text-custom-green-normal"
-                    : "text-red-600"
-                    } font-semibold`}
-            >
-                {status}
-            </td>
-            {/* Sold */}
-            <td className="py-3">{sold}</td>
-            {/* View */}
-            <td className="py-3">{view}</td>
-
-        </tr>
-    );
+  return (
+    <tr
+      key={no}
+      className="w-full overflow-x-scroll rounded-[20px] bg-custom-purple-300 font-poppins text-sm text-white lg:text-base"
+    >
+      {/* Numbering */}
+      <td className="py-3">{no}</td>
+      {/* Image */}
+      <td className="w-[120px] py-3">
+        <Image
+          alt={eventName + " Image"}
+          src={imageUrl}
+          width={120}
+          height={60}
+          className="h-[60px] w-[120px] rounded-[10px] object-cover object-center"
+        />
+      </td>
+      {/* Event Name */}
+      {/* Event Name */}
+      <td className="min-w-[140px] px-2 py-3">{eventName}</td>
+      {/* Status */}
+      <td
+        className={`capitalize ${
+          status.toLowerCase() === "active"
+            ? "text-custom-green-normal"
+            : "text-red-600"
+        } font-semibold`}
+      >
+        {status}
+      </td>
+      {/* Sold */}
+      <td className="py-3">{sold}</td>
+      {/* View */}
+      <td className="py-3">{view}</td>
+    </tr>
+  );
 };
 export default TicketList;
